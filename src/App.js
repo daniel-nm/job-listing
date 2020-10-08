690d8f11f6f22e345465efb1d577448e315dfd85
Daniel <daniel.naranjo.merino@gmail.com>
2020-10-08 08:32:23 +0200

    Setup container styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Listing from './components/Listing';
 
 function App() {
   return (
     <div className="App">
-
+      <Listing />
     </div>
   );
 }
--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../styles/layout.css';
+import '../styles/components/Listing.css';
+import {ReactComponent as RemoveIcon} from '../images/icon-remove.svg';
+
+// Import JSON data
+import { jobs } from '../data';
 
 function Listing() {
+
+  // Set job list initial status
+  const [jobListing, setJobListing] = useState([]);
+  
+  useEffect(() => {
+    setJobListing(jobs);
+  }, []);
+
+
   return (
     <div>
-      
+      {jobListing.length > 0 && jobListing.map((job) => {
+          return (
+            <p>Test</p>
+          )
+        })}
     </div>
   )
 }
